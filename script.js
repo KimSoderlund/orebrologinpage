1a63f584372846d18ca123b4296bd913a021a147
Kim Söderlund <kimsoderlund33@gmail.com>
2026-02-11 11:45:36 +0100

    Added create account code

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-// to create account button code
+// login page create account button code
 const registerBtn = document.getElementById("signup-btn");
 registerBtn.addEventListener("click", function () {
   showScreen("create-account-screen");
+});
+
+// Create user button code
+const createUserBtn = document.getElementById("create-user-btn");
+
+createUserBtn.addEventListener("click", function () {
+  const newUsernameInput = document.getElementById("new-user").value;
+  const newPasswordInput = document.getElementById("new-password").value;
+
+  if (newUsernameInput === "" || newPasswordInput === "") {
+    alert("Username and password cannot be empty.");
+    return;
+  }
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const userExists = users.some((user) => user.username === newUsernameInput);
+
+  if (userExists) {
+    alert("Username already exists.");
+    return;
+  }
+
+  users.push({
+    username: newUsernameInput,
+    password: newPasswordInput,
+  });
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  showScreen("login-screen");
 });
 
 // Screen Controller
